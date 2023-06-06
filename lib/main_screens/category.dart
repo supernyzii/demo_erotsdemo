da6264c2d999299f2c7a66deda5c6b6a921a2ebe
supernyzii <supernyzii@gmail.com>
2023-06-06 20:38:56 +0000

    build widget for the widget inside the stack to make it more concise

--- lib/main_screens/category.dart
+++ lib/main_screens/category.dart
@@ -11,6 +11,7 @@ class CategoryScreen extends StatefulWidget {
 class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return  Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -22,21 +23,29 @@ class _CategoryScreenState extends State<CategoryScreen> {
           Positioned(
             bottom: 0,
             left: 0,
-            child: Container(
-              height: MediaQuery.of(context).size.height * 0.8,
-              width:  MediaQuery.of(context).size.width * 0.2,
-              color: Colors.grey.shade300,
-            ),),
+            child: sideNavigator(size) ,),
           Positioned(
             bottom: 0,
             right: 0,
-            child: Container(
-              height: MediaQuery.of(context).size.height * 0.8,
-              width: MediaQuery.of(context).size.width * 0.8,
-              color: Colors.white,
-            ),),
+            child: categView(size),),
         ],
       )
+    );
+  }
+
+  Widget sideNavigator ( Size size) {
+   return  Container(
+      height: size.height * 0.8,
+      width:  size.width * 0.2,
+      color: Colors.grey.shade300,
+    );
+  }
+
+  Widget categView( Size size){
+     return Container(
+      height: size.height * 0.8,
+      width: size.width * 0.8,
+      color: Colors.white,
     );
   }
 }
