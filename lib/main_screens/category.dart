0256c6110fed6fc7cf390812df2f7f944a51e41d
supernyzii <supernyzii@gmail.com>
2023-06-07 09:50:40 +0000

    added labels to the side navigator change the color of a label on click using the GestureDetector and its onTap function
    we use a loop to help in the process ... the loop goes through all the list and assign boolean false to it

--- lib/main_screens/category.dart
+++ lib/main_screens/category.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:istorebtaxx/widgets/fake_search.dart';
 
+List<ItemsData> items = [
+  ItemsData(label: 'men'),
+  ItemsData(label: 'women'),
+  ItemsData(label: 'shoes'),
+  ItemsData(label: 'bags'),
+  ItemsData(label: 'electronics'),
+  ItemsData(label: 'accessories'),
+  ItemsData(label: 'home & garden'),
+  ItemsData(label: 'kids'),
+  ItemsData(label: 'beauty'),
+];
+
 class CategoryScreen extends StatefulWidget {
   const CategoryScreen({Key? key}) : super(key: key);
 
@@ -23,21 +35,41 @@ class _CategoryScreenState extends State<CategoryScreen> {
           Positioned(
             bottom: 0,
             left: 0,
-            child: sideNavigator(size) ,),
+            child: sideNavigator(size)),
           Positioned(
             bottom: 0,
             right: 0,
-            child: categView(size),),
+            child: categView(size)),
         ],
-      )
+      ),
     );
   }
 
   Widget sideNavigator ( Size size) {
-   return  Container(
+   return  SizedBox(
       height: size.height * 0.8,
       width:  size.width * 0.2,
-      color: Colors.grey.shade300,
+      child: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index){
+            return  GestureDetector(
+              onTap: () {
+                for (var element in items){
+                  element.isSelected = false;
+                }
+                setState(() {
+                  items[index].isSelected = true;
+                });
+              },
+              child: Container(
+                color: items[index].isSelected == true ? Colors.white : Colors.grey.shade300,
+                height: 100,
+                child: Center (
+                  child: Text(items[index].label),
+                ),
+              ),
+            );
+      }),
     );
   }
 
@@ -48,4 +80,10 @@ class _CategoryScreenState extends State<CategoryScreen> {
       color: Colors.white,
     );
   }
+}
+
+class ItemsData{
+  String label;
+  bool isSelected;
+  ItemsData({required this.label, this.isSelected = false});
 }
